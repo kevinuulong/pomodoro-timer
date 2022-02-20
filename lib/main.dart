8d2b54716581ed8f9a75efb698745dce76b5e551
Kevin Long <kevin.uu.long@gmail.com>
2022-02-20 10:53:55 -0500

    Bottom Navbar and Quick Start

---
+++ lib/main.dart
@@ -0,0 +1,91 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Startup Name Generator',
+      home: RandomWords(),
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({Key? key}) : super(key: key);
+
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18);
+  int index = 0;
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16),
+      itemBuilder: (context, i) {
+        if (i.isOdd) {
+          return const Divider();
+        }
+
+        final index = i ~/ 2;
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+      },
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Startup Name Generator'),
+        ),
+        body: _buildSuggestions(),
+        bottomNavigationBar: NavigationBarTheme(
+            data: NavigationBarThemeData(
+                indicatorColor: Colors.blue.shade100,
+                labelTextStyle: MaterialStateProperty.all(
+                  TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
+                )),
+            child: NavigationBar(
+              onDestinationSelected: (index) => 
+                setState(() {
+                  this.index = index;
+                }),
+              selectedIndex: index,
+              destinations: const [
+                NavigationDestination(
+                  icon: Icon(Icons.work_rounded),
+                  label: 'Work',
+                ),
+                NavigationDestination(
+                  icon: Icon(Icons.notifications_none_rounded),
+                  label: 'Short Break',
+                ),
+                NavigationDestination(
+                    icon: Icon(Icons.bed_rounded), label: 'Long Break')
+              ],
+            )));
+  }
+}
